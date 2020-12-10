fad5390abe81f1861b33932612e53dfe8a1d9d3b
makxxii <maksimya@bk.ru>
2020-12-11 02:23:03 +0300

    Сделал все для обучения кроме мигания кнопки

--- Assets/Scripts/MoveSequenceChecker.cs
+++ Assets/Scripts/MoveSequenceChecker.cs
@@ -1,38 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MoveSequenceChecker : MonoBehaviour
 {
+
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject nextLVLButton;
+
+    [SerializeField] private Text loseText;
+
     [SerializeField] private GameObject playerPrefab;
 
     [SerializeField] private List<Slot> endSlots;
 
+    [SerializeField] private GameObject blackoutPanel;
+    [SerializeField] private GameObject particles;
+    [SerializeField] private GameObject likesAndComentsPanel;
+
+    [SerializeField] private Text likesCountPlace;
+    [SerializeField] private Text comentsCountPlace;
+
+    [SerializeField] private int likesCount = 4000;
+    [SerializeField] private int comentsCount = 500;
+    [SerializeField] private float likesAddSpeed = 0.01f;
+    [SerializeField] private float comentsAddSpeed = 0.1f;
+
     private int animIndex = 0;
+
+    private Coroutine movesChek;
 
     public void Start()
     {
 
     }
 
+    public void Update()
+    {
+        if (IsSlotsIsNull())
+        {
+            playButton.SetActive(true);
+        }
+    }
+
+
     public void CorrectSequenceCheck()
     {
         if (IsSlotsIsNull())
         {
-            if (MoveSequence())
-            {
-                TeacherController.Instance.StopDance();
-                StartCoroutine(SyncDances());
+            //if (MoveSequence())
+            //{
+            //    TeacherController.Instance.StopDance();
+            //    StartCoroutine(SyncDances());
 
-                Debug.Log("vin!");
-            }
-            else
-            {
-                StartCoroutine(PlayerAnsyncDance());
+            //    Debug.Log("vin!");
+            //}
+            //else
+            //{
+            //    StartCoroutine(PlayerAnsyncDance());
 
-                Debug.Log("lose!:(");
-            }
+            //    Debug.Log("lose!:(");
+            //}
+            movesChek = StartCoroutine(MovesChek());
         }
         else
         {
@@ -64,7 +95,6 @@ public class MoveSequenceChecker : MonoBehaviour
 
         return true;
     }
-
     private bool IsSlotsIsNull()
     {
         foreach (var slot in endSlots)
@@ -75,6 +105,45 @@ public class MoveSequenceChecker : MonoBehaviour
 
         return true;
     }
+
+    IEnumerator MovesChek()
+    {
+        TeacherController.Instance.StopDance();
+        //TeacherController.Instance.doDance = false;
+
+        int i = 0;
+        foreach (var slot in endSlots)
+        {
+            yield return new WaitForSeconds(TeacherController.Instance.teacher.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+
+            var playerMove = slot.GetCurrentCard().data.danceParameterName;
+            var teacherMove = TeacherController.Instance.moveSequence[i];
+
+            if (playerMove != teacherMove)
+            {
+                slot.GetComponent<Image>().color = Color.red;
+                Lose();
+            }
+            else
+            {
+                slot.GetComponent<Image>().color = Color.green;
+
+                playerPrefab.GetComponent<Animator>().SetTrigger(TeacherController.Instance.moveSequence[i]);
+                TeacherController.Instance.teacher.GetComponent<Animator>().SetTrigger(TeacherController.Instance.moveSequence[i]);
+            }
+
+
+            if (i >= 3)
+            {
+                Victory();
+                i = 0;
+            }
+
+            i++;
+        }
+    }
+
+
 
     IEnumerator SyncDances()
     {
@@ -112,5 +181,51 @@ public class MoveSequenceChecker : MonoBehaviour
         }
     }
 
+    public void Lose()
+    {
+        StopCoroutine(movesChek);
+        TeacherController.Instance.doDance = false;
+        TeacherController.Instance.StopDance();
+        playerPrefab.GetComponent<Animator>().SetTrigger("Sad");
+        TeacherController.Instance.teacher.GetComponent<Animator>().SetTrigger("Sad");
+        loseText.gameObject.SetActive(true);
+    }
 
+    public void Victory()
+    {
+        loseText.gameObject.SetActive(false);
+
+        particles.SetActive(true);
+        blackoutPanel.SetActive(true);
+        likesAndComentsPanel.SetActive(true);
+        nextLVLButton.SetActive(true);
+
+        StartCoroutine(AddLikes());
+        StartCoroutine(AddComents());
+    }
+
+    IEnumerator AddLikes()
+    {
+        for (int i = 1; i <= likesCount; i++)
+        {
+            likesCountPlace.text = i.ToString();
+
+            yield return new WaitForSeconds(likesAddSpeed);
+        }
+    }
+
+    IEnumerator AddComents()
+    {
+        for (int i = 1; i <= comentsCount; i++)
+        {
+            comentsCountPlace.text = i.ToString();
+
+            yield return new WaitForSeconds(likesAddSpeed);
+        }
+    }
+
+    public void NextLVL()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
--- Assets/Scripts/TeacherController.cs
+++ Assets/Scripts/TeacherController.cs
@@ -3,37 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
+
 public class TeacherController : MonoBehaviour
 {
     public static TeacherController Instance { get; private set; }
 
     public GameObject teacher;
     public GameObject Player;
+    public GameObject availableCards;
+    public GameObject slots;
+    public GameObject handHintPrefab;
+
+    public Button reloadScene, ready;
 
     public List<string> moveSequence;
+
+    public float writingTextSpeed = 0.1f;
+    public float phraseDelay = 5f;
+
+    public Text teacherPhrasePlace;
+    public Text hint;
+
+    public string teacherPhrase1;
+    public string teacherPhrase2;
+    public bool doDance = true;
 
     private Coroutine danceCoroutine;
 
     private int animIndex = 0;
     private string currentAnimation;
 
+    private bool isEndSpeach = false;
+    private bool isLastMove = true;
+
     private void Awake()
     {
         Instance = this;
         teacher = transform.gameObject;
+
+
     }
 
     private void Start()
     {
-        if (moveSequence == null)
-        {
-            moveSequence.Add("normal");
-            moveSequence.Add("happy");
-            moveSequence.Add("angry");
-            moveSequence.Add("dead");
-        }
-
-        danceCoroutine = StartCoroutine(TeacherDancing());
+        StartCoroutine(TextWriting(teacherPhrase1));
+        //Time.timeScale = 10f;
+        //danceCoroutine = StartCoroutine(TeacherDancing());
     }
 
     internal void StopDance()
@@ -41,24 +57,106 @@ public class TeacherController : MonoBehaviour
         StopCoroutine(danceCoroutine);
     }
 
+    public void ReloadClip()
+    {
+        StopCoroutine(TextWriting(teacherPhrase1));
+        StartCoroutine(TextWriting(teacherPhrase1));
+    }
+
+    public void Ready()
+    {
+        teacherPhrasePlace.text = "";
+        //teacherPhrasePlace.gameObject.SetActive(false);
+        reloadScene.gameObject.SetActive(false);
+        ready.gameObject.SetActive(false);
+
+        danceCoroutine = StartCoroutine(TeacherDancing());
+    }
+
     public IEnumerator TeacherDancing()
     {
-        while (true)
+        while (doDance)
         {
             yield return new WaitForSeconds(GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
 
             GetComponent<Animator>().SetTrigger(moveSequence[animIndex]);
-
+            teacherPhrasePlace.text = "Movement №" + (animIndex + 1) + ":" + moveSequence[animIndex];
             animIndex++;
 
             if (animIndex >= 4)
             {
-                transform.DOMove(new Vector3(-2.17f, 1.52f, 10.47f),3f).SetEase(Ease.Flash).OnComplete(
-                    () => Player.transform.DOMove(new Vector3(0.05f, -0.92f, 2.94f), 3f).SetEase(Ease.Flash));
+
+                if (isLastMove)
+                {
+                    Time.timeScale = 1f;
+                    yield return new WaitForSeconds(GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+
+                    transform.DOMove(new Vector3(-2.17f, 1.52f, 10.47f), 3f).SetEase(Ease.Flash).OnComplete(
+                                        () => Player.transform.DOMove(new Vector3(0.05f, -0.92f, 2.94f), 3f).SetEase(Ease.Flash)
+                        .OnComplete(() => SetSlotsActive()));
+
+
+                    teacherPhrasePlace.gameObject.SetActive(false);
+
+
+
+                    isLastMove = false;
+                }
+
 
                 animIndex = 0;
             }
         }
     }
+
+    public void SetSlotsActive()
+    {
+        availableCards.SetActive(true);
+        slots.SetActive(true);
+        hint.gameObject.SetActive(true);
+
+        handHintPrefab.SetActive(true);
+        handHintPrefab.transform.DOLocalMove(new Vector3(219f, 661f, 0f), 3f).SetEase(Ease.Flash)
+            .OnComplete(() => handHintPrefab.SetActive(false));
+    }
+
+    public IEnumerator TextWriting(string Phrase)
+    {
+        reloadScene.enabled = false;
+        ready.enabled = false;
+
+        teacherPhrasePlace.text = "";
+
+        foreach (char letter in Phrase)
+        {
+            teacherPhrasePlace.text += letter;
+
+            yield return new WaitForSeconds(writingTextSpeed);
+        }
+
+        yield return new WaitForSeconds(phraseDelay);
+
+        if (!isEndSpeach)
+        {
+            StartCoroutine(TextWriting(teacherPhrase2));
+            isEndSpeach = true;
+        }
+        else
+        {
+            teacherPhrasePlace.text = "";
+
+            foreach (char letter in "Ready?")
+            {
+                teacherPhrasePlace.text += letter;
+
+                yield return new WaitForSeconds(writingTextSpeed);
+            }
+
+            reloadScene.enabled = true;
+            ready.enabled = true;
+        }
+    }
+
+
 }
 
